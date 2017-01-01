b53ace4f5231da165e27f73d3426e7e3027b9416
Andrea <takamiya.andrea@gmail.com>
2016-12-31 18:39:27 -1000

    Created the new file body with dynamic fields

--- server.js
+++ server.js
@@ -3,13 +3,6 @@ const PORT = process.env.PORT || 1234;
 const fs = require('fs');
 const querystring = require('querystring');
 
-// var elementName = '';
-// var elementSymbol = '';
-// var elementAtomicNumber = '';
-// var elementDescription = '';
-// var newFileName = '';
-
-
 const server = http.createServer( (req, res) => {
 
   var options = {
@@ -24,22 +17,32 @@ const server = http.createServer( (req, res) => {
   req.on('data', (chunk) => {
     reqBody += chunk;
 
-  var reqSplit = querystring.parse(reqBody);
-  var elementName = reqSplit.elementName;
-  var newFileName = elementName + ".html";
-  var elementSymbol = reqSplit.elementSymbol;
-  var elementAtomicNumber = reqSplit.elementAtomicNumber;
-  var elementDescription = reqSplit.elementDescription;
+    var reqSplit = querystring.parse(reqBody);
+    var elementName = reqSplit.elementName;
+    var newFileName = elementName + ".html";
+    var elementSymbol = reqSplit.elementSymbol;
+    var elementAtomicNumber = reqSplit.elementAtomicNumber;
+    var elementDescription = reqSplit.elementDescription;
+    var newFileBody = `<html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>The Elements - ${elementName}</title>
+      <link rel="stylesheet" href="/css/styles.css">
+    </head>
+    <body>
+      <h1>${elementName}</h1>
+      <h2>${elementSymbol}</h2>
+      <h3>Atomic number ${elementAtomicNumber}</h3>
+      <p>${elementName} is a chemical element with symbol ${elementSymbol} and atomic number ${elementAtomicNumber}. Because boron is produced entirely by cosmic ray spallation and not by stellar nucleosynthesis it is a low-abundance element in both the Solar system and the Earth's crust.[12] Boron is concentrated on Earth by the water-solubility of its more common naturally occurring compounds, the borate minerals. These are mined industrially as evaporites, such as borax and kernite. The largest proven boron deposits are in Turkey, which is also the largest producer of boron minerals.</p>
+      <p><a href="/">back</a></p>
+    </body>
+    </html>`;
 
-  res.write('YES');
-  console.log(newFileName);
-  fs.writeFile(newFileName, 'test', (err) => {
-    // if (err) throw err;
-    console.log('It\'s saved!');
+    fs.writeFile(newFileName, newFileBody, (err) => {
+      // if (err) throw err;
+      console.log('It\'s saved!');
+    });
   });
-  });
-
-
 
   req.on('end', () => {
 
