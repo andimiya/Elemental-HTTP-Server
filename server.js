dc11500a6cfb5cc4a84fe3080ff9a38ade7300fe
Andrea <takamiya.andrea@gmail.com>
2016-12-31 19:48:43 -1000

    Saving progress, moved stuff around to first look for either GET or POST first

--- server.js
+++ server.js
@@ -5,106 +5,126 @@ const querystring = require('querystring');
 
 const server = http.createServer( (req, res) => {
 
-  var options = {
-    hostname: 'localhost',
-    port: 1234,
-    path: '/elements',
-    method: 'POST',
-  };
+  console.log(req.method, 'requestmethod');
 
-  var reqBody = '';
-  req.setEncoding('utf8');
-  req.on('data', (chunk) => {
-    reqBody += chunk;
+  if (req.method === 'POST') {
 
-    var reqSplit = querystring.parse(reqBody);
-    var elementName = reqSplit.elementName;
-    var newFileName = elementName + ".html";
-    var elementSymbol = reqSplit.elementSymbol;
-    var elementAtomicNumber = reqSplit.elementAtomicNumber;
-    var elementDescription = reqSplit.elementDescription;
-    var newFileBody = `<html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>The Elements - ${elementName}</title>
-      <link rel="stylesheet" href="/css/styles.css">
-    </head>
-    <body>
-      <h1>${elementName}</h1>
-      <h2>${elementSymbol}</h2>
-      <h3>Atomic number ${elementAtomicNumber}</h3>
-      <p>${elementName} is a chemical element with symbol ${elementSymbol} and atomic number ${elementAtomicNumber}. Because boron is produced entirely by cosmic ray spallation and not by stellar nucleosynthesis it is a low-abundance element in both the Solar system and the Earth's crust.[12] Boron is concentrated on Earth by the water-solubility of its more common naturally occurring compounds, the borate minerals. These are mined industrially as evaporites, such as borax and kernite. The largest proven boron deposits are in Turkey, which is also the largest producer of boron minerals.</p>
-      <p><a href="/">back</a></p>
-    </body>
-    </html>`;
+    if (req.url === '/elements'){
 
-    fs.writeFile(newFileName, newFileBody, (err) => {
-      if (err) {
-        console.log('New file creation has failed');
-      };
-      console.log('New file has been created!');
-    });
+      console.log('elementsFile');
 
-    res.writeHead(200, {
-      'Content-Type': 'application/json',
-      'success': true
-    });
-  });
+      var reqBody = '';
+      req.setEncoding('utf8');
+      req.on('data', (chunk) => {
+        reqBody += chunk;
+
+        var reqSplit = querystring.parse(reqBody);
+        var elementName = reqSplit.elementName;
+        var newFileName = elementName + ".html";
+        var elementSymbol = reqSplit.elementSymbol;
+        var elementAtomicNumber = reqSplit.elementAtomicNumber;
+        var elementDescription = reqSplit.elementDescription;
+        var newFileBody = `<html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <title>The Elements - ${elementName}</title>
+          <link rel="stylesheet" href="/css/styles.css">
+        </head>
+        <body>
+          <h1>${elementName}</h1>
+          <h2>${elementSymbol}</h2>
+          <h3>Atomic number ${elementAtomicNumber}</h3>
+          <p>${elementName} is a chemical element with symbol ${elementSymbol} and atomic number ${elementAtomicNumber}. Because boron is produced entirely by cosmic ray spallation and not by stellar nucleosynthesis it is a low-abundance element in both the Solar system and the Earth's crust.[12] Boron is concentrated on Earth by the water-solubility of its more common naturally occurring compounds, the borate minerals. These are mined industrially as evaporites, such as borax and kernite. The largest proven boron deposits are in Turkey, which is also the largest producer of boron minerals.</p>
+          <p><a href="/">back</a></p>
+        </body>
+        </html>`;
+
+        res.writeHead(200, {
+          'Content-Type': 'application/json',
+          'success': true
+        });
+
+        fs.writeFile(newFileName, newFileBody, (err) => {
+          if (err) {
+            console.log('New file creation has failed');
+          };
+          console.log('New file has been created!');
+        });
 
 
-  req.on('end', () => {
 
-    if (req.url === '/') {
-      fs.readFile('./public/index.html', (err, content) => {
-        if (err) {
-          res.statusCode = 500;
-          res.write('Server fault occured\n');
-        }
-        res.write(content);
-        res.end();
-      });
-    }
-    else if (req.url === '/hydrogen') {
-      fs.readFile('./public/hydrogen.html', (err, content) => {
-        if (err) {
-          res.statusCode = 500;
-          res.write('Server fault occured\n');
-        }
-        res.write(content);
-        res.end();
-      });
-    }
-    else if (req.url === '/helium') {
-      fs.readFile('./public/helium.html', (err, content) => {
-        if (err) {
-          res.statusCode = 500;
-          res.write('Server fault occured\n');
-        }
-        res.write(content);
-        res.end();
-      });
-    }
-    else if (req.url === '/css/styles.css') {
-      fs.readFile('./public/css/styles.css', (err, content) => {
-        if (err) {
-          res.statusCode = 500;
-          res.write('Server fault occured\n');
-        }
-        res.write(content);
-        res.end();
+        res.end('post finished');
       });
     }
     else {
-      fs.readFile('./public/404.html', (err, content) => {
-        if (err) {
-          res.statusCode = 500;
-          res.write('Server fault occured\n');
-        }
-        res.write(content);
-        res.end();
-      });
+      console.log('Only a ./elements file can be requested');
     }
-  });
+  }
+
+  else if (req.method === 'GET') {
+
+
+    req.on('end', () => {
+      console.log('GET')
+
+      if (req.url === '/') {
+        fs.readFile('./public/index.html', (err, content) => {
+          if (err) {
+            res.statusCode = 500;
+            res.write('Server fault occured\n');
+          }
+          res.write(content);
+          res.end();
+        });
+      }
+      else if (req.url === '/hydrogen') {
+        fs.readFile('./public/hydrogen.html', (err, content) => {
+          if (err) {
+            res.statusCode = 500;
+            res.write('Server fault occured\n');
+          }
+          res.write(content);
+          res.end();
+        });
+      }
+      else if (req.url === '/helium') {
+        fs.readFile('./public/helium.html', (err, content) => {
+          if (err) {
+            res.statusCode = 500;
+            res.write('Server fault occured\n');
+          }
+          res.write(content);
+          res.end();
+        });
+      }
+      else if (req.url === '/css/styles.css') {
+        fs.readFile('./public/css/styles.css', (err, content) => {
+          if (err) {
+            res.statusCode = 500;
+            res.write('Server fault occured\n');
+          }
+          res.write(content);
+          res.end();
+        });
+      }
+
+      else {
+        fs.readFile('./public/404.html', (err, content) => {
+          if (err) {
+            res.statusCode = 500;
+            res.write('Server fault occured\n');
+          }
+          res.write(content);
+          res.end();
+        });
+      }
+    });
+  }
+
+  else {
+    console.log('neither GET or POST');
+  }
+
 });
 
 server.listen(PORT, () => {
